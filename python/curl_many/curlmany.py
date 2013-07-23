739b61de6d5ea737cc9f7daffae6498a38746004
jag <jag.srawan@ampush.com>
2013-07-23 11:37:11 -0700

    curlmany.py for gets

--- python/curl_many/curlmany.py
+++ python/curl_many/curlmany.py
@@ -3,28 +3,29 @@ from itertools import izip
 
 
 filename = "example1.csv"
-reader = csv.reader(open(filename))
-try:
-   for row in reader:
-        all_items = row.split(',')
-        number = all_items[0]
-        method = all_items[1]
-        url = all_items[2]
-        all_params = all_items[3:]
-	    if len(all_params) % 2 != 0:
-            raise Exception('uneven number of params')
-        i = iter(all_params)
-        all_params_pair = (izip(i, i))
-        param_string = "&".join( [ "%=%" % (params[0],params[1]) for params in all_params_pair] )
-        final_url = "%?%" % (url,param_string))
-        req = urllib2.Request(final_url)
-        try:
-        	response = urllib2.urlopen(req)
-            file_name = "%.rsp" % number
-       		local_file = open(file_name, "w" + file_mode)
-		    #Write to our local file
-    		local_file.write(response.read())
-    		local_file.close()
-        catch Exception,e:
-            print e
+reader = csv.reader(open(filename), )
+next(reader, None)  # skip the headers
+for all_items in reader:
+    number = all_items[0]
+    method = all_items[1]
+    url = all_items[2]
+    all_params = all_items[3:]
+    if len(all_params) % 2 != 0:
+        raise Exception('uneven number of params')
+    i = iter(all_params)
+    all_params_pair = list(izip(i, i))
+    #print all_params_pair
+    param_string = "&".join( [ (param[0] + "=" +param[1]) for param in all_params_pair] )
+    final_url = url + "?" + param_string
+    print final_url
+    req = urllib2.Request(final_url)
+    try:
+        response = urllib2.urlopen(req)
+        file_name =  number + ".rsp"
+        local_file = open(file_name, "w")
+	    #Write to our local file
+        local_file.write(response.read())
+        local_file.close()
+    except Exception,e:
+        print e
 
