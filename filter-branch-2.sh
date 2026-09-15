58eb9446dad1a04755084c4d4e562a85cb7bca75
jsrawan-mobo <jsrawan@gmail.com>
2026-09-15 07:18:24 -0700

    obselete scripts

--- filter-branch-2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-git filter-branch -f --env-filter "
-    GIT_AUTHOR_NAME='Jag Srawan'
-    GIT_AUTHOR_EMAIL='jsrawan@gmail.com'
-    GIT_COMMITTER_NAME='jsrawan-mobo'
-    GIT_COMMITTER_EMAIL='jsrawan@gmail.com'
-  " HEAD
